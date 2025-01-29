54216dd9b09f63716750c1de545ca58eccf6ff18
Michael DeBellis <mdebellissf@gmail.com>
2025-01-29 10:12:09 -0800

    Created basic parse_authors
    And test data

--- src/parse_authors.py
+++ src/parse_authors.py
@@ -15,3 +15,19 @@ def create_author(last_name, first_name=None, middle_name=None):
     if first_name is not None: put_value(author_iri, first_name_prop, first_name)
     return author_iri
 
+
+def parse_authors(authors_string):
+    # Just a basic start, ignoring middle names and many alternative patterns
+    authors = authors_string.split(";")  # Split authors by semicolon
+    author_list = []
+    for author in authors:
+        name_parts = author.split(",")  # Split each author by comma
+        last = name_parts[0].strip()  # Get the last name and strip whitespace
+        first = name_parts[1].strip()
+        author_iri = create_author(last_name=last, first_name=first)
+        author_list.append(author_iri)  # Add the created author's IRI to the list
+    return author_list
+
+
+
+
--- tests/parse_authors_test.py
+++ tests/parse_authors_test.py
@@ -1,7 +1,15 @@
-from src.parse_authors import create_author, last_name_prop, first_name_prop
+from src.parse_authors import *
 from src.ag_api import *
 
+authors_string = "Chomsky, Noam; Turing, Allen"
 fs_fitzgerald = create_author("Fitzgerald", "F.", "Scott")
 print(fs_fitzgerald)
 print(get_value(fs_fitzgerald,last_name_prop))
 print(get_value(fs_fitzgerald,first_name_prop))
+author_list = parse_authors(authors_string)
+first_author = author_list[0]
+print(get_value(first_author,last_name_prop))
+print(get_value(first_author,first_name_prop))
+second_author = author_list[1]
+print(get_value(second_author,last_name_prop))
+print(get_value(second_author,first_name_prop))
