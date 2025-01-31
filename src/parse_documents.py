6a97ca4e0f6bb1ce9e938914dad51677fa8cc1e5
Michael DeBellis <mdebellissf@gmail.com>
2025-01-31 07:57:11 -0800

    changed ontology and made new branch (#1)
    * changed ontology and made new branch
    
    1) Got rid of conn create at the top as that is now handled in the ag_api.py file. From now on, I'll just call that api. 
    2) Removed convert_to_string, domain_ont_str function, rdfs_label_prop as they are now in the api. 
    3)Removed create_sub_section, display_sub_section, section_class,   sub_section_prop as they are no longer needed since we are storing all the strings on the document instance. 
    4) Changed string of document_class, text_prop for new ontology IRI. Also, changed the ontology a bit. Gist (the upper model we are starting from) has a class called Content that I was using as the superclass for all documents but I realized that class has a lot of sub classes that may be useful at some point but that won't have content we need to scrape from the Internet. It was easier to have one class that could be the superclass for all objects that could have scrapable (and vectorizable) content so I made Document a subclass of Content and made existing classes like Web_Page and Journal_Article, subclasses of Document. 
    5) Changed ontology to use the Bib uri property. Better to use the standard. I had forgotten about it and was just using a url property I created in the domain ontology.
    
    * Update climate_obstruction_1-30-25.ttl

--- src/parse_documents.py
+++ src/parse_documents.py
@@ -1,5 +1,4 @@
-from franz.openrdf.connect import ag_connect
-from franz.openrdf.vocabulary import RDF
+from src.ag_api import *
 import uuid
 import document_parser as dp
 import os
@@ -19,15 +18,11 @@ from selenium.webdriver.chrome.options import Options
 
 
 # Create a connection object and bind to conn. The conn object is used to connect with an AllegroGraph repository
-conn = ag_connect(repo='drmo', host='localhost', port=10035, user='test', password='xyzzy')
+
 
 # Set up variables bound to various classes and properties needed for this file
-section_class = conn.createURI("http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#Section")
-document_class = conn.createURI("http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#Document")
-domain_ont_str = "http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#"
-rdfs_label_prop = conn.createURI("http://www.w3.org/2000/01/rdf-schema#label")
-text_prop = conn.createURI("http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#text")
-sub_section_prop = conn.createURI("http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#hasSubSection")
+document_class = conn.createURI("https://www.michaeldebellis.com/climate_obstruction/Document")
+text_prop = conn.createURI("https://www.michaeldebellis.com/climate_obstruction/text")
 iri_prop = conn.createURI("http://purl.org/ontology/bibo/uri")
 heading_prop = conn.createURI("http://www.semanticweb.org/ontologies/2022/titutuli/nivedita/drmo#heading")
 source_document = conn.createURI(
@@ -48,70 +43,12 @@ service = Service(ChromeDriverManager().install())
 
 
 
-
-
-# Gets the value of a single valued property using the IRI name of the instance and the IRI name of the property
-# If the property has multiple values prints a warning and returns the first one
-# If the property has no value returns None Note: if not sure whether property has multiple values, best to use get_values
-def get_value(instance, owl_property, debug=False):
-    if instance is None:
-        if debug:
-            print("Error no object with iri name: {iri_name}")
-        return None
-    statements = conn.getStatements(instance, owl_property, None)
-    with statements:
-        for statement in statements:
-            if len(statements) > 1:
-                if debug:
-                    print(f'Warning: two or more values for property: {owl_property}. Using first one.')
-                return statement.getObject()
-            elif len(statements) == 1:
-                return statement.getObject()
-    if debug:
-        print(f'Warning: No property value for: {instance, owl_property}.')
-    return None
-
-# When getting values that are datatypes there is all sorts of extra stuff we usually want to strip out
-# E.g., in the dest data below the result of get_value("MichaelDeBellis", "email") will be: "mdebellissf@gmail.com"^^<http://www.w3.org/2001/XMLSchema#anyURI>
-# this should strip out the datatype and extra string characters so will return mdebelissf@gmail.com
-def convert_to_string (literal):
-        literal = str(literal)
-        if "^" in literal:
-            literal = literal.replace(literal[literal.find("^") + len("^"):], '') #remove the datatype
-            literal = literal[1:len(literal) - 2] # remove the string characters and the remaining ^
-        return literal
-
-
 def get_url_for_document(document):
     url_string = get_value(document, iri_prop)
     if url_string is None:
         return None
     else:
         return convert_to_string(url_string)
-
-def create_sub_section(document, heading=None, text=None):
-    sub_section = conn.createURI(domain_ont_str + str(uuid.uuid4()))
-    conn.add(sub_section, RDF.TYPE, section_class)
-    if heading is not None:
-        conn.add(sub_section, heading_prop, heading)
-        conn.add(sub_section, rdfs_label_prop, heading)
-    if text is not None:
-        conn.add(sub_section, text_prop, text)
-    conn.add(document, sub_section_prop, sub_section)
-    return sub_section
-
-
-# Needed to create a set because graph was returning duplicates, not sure why but the documentation says that is possible
-# See: https://franz.com/agraph/support/documentation/current/python/api.html#repositoryresult-class
-# It may have been an issue in the ontology I was working on. I hadn't thought of this at the time
-# but it's possible in buggy versions of the code I asserted new values on the source object rather than the TestDocument
-# but I don't thin so.
-def display_sub_section(section):
-    print(get_value(section, heading_prop))
-    sub_section_statements = conn.getStatements(section, sub_section_prop, None)
-    section_set = set([section_statement.getObject() for section_statement in sub_section_statements])
-    for section in section_set:
-        display_sub_section(section)
 
 
 # This iterates over all documents and checks if the document has sub sections or a value for the text field
@@ -158,6 +95,16 @@ def extract_sciencedirect_links(csv_file_path):
                 links.extend(sciencedirect_link_pattern.findall(cell))
     return links
 
+def create_sub_section(document, heading=None, text=None):
+    sub_section = conn.createURI(domain_ont_str + str(uuid.uuid4()))
+    conn.add(sub_section, RDF.TYPE, section_class)
+    if heading is not None:
+        conn.add(sub_section, heading_prop, heading)
+        conn.add(sub_section, rdfs_label_prop, heading)
+    if text is not None:
+        conn.add(sub_section, text_prop, text)
+    conn.add(document, sub_section_prop, sub_section)
+    return sub_section
 
 # This function is used to build the sections for a document. It uses the document parser to get the sections
 
