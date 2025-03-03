80ab2f1cf932383f1c47344f3901198b15d57348
Michael DeBellis <mdebellissf@gmail.com>
2025-03-03 14:20:57 -0800

    Update parse_authors.py

--- src/parse_authors.py
+++ src/parse_authors.py
@@ -2,7 +2,6 @@ import uuid
 from src.ag_api import *
 from nameparser import HumanName
 
-
 last_name_prop = find_property(make_ontology_iri("last_name"))
 first_name_prop = find_property(make_ontology_iri("first_name"))
 middle_name_prop = find_property(make_ontology_iri("middle_name"))
@@ -11,17 +10,29 @@ document_class = find_class(make_ontology_iri("Document"))
 author_string_property = find_property(make_ontology_iri("author_string"))
 author_property = find_property(make_ontology_iri("has_author"))
 
+class Author:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    def __eq__(self, other):
+        return isinstance(other, Author) and self.first_name == other.first_name and self.last_name == other.last_name
+    def __repr__(self):
+        return f"Author('{self.first_name}', '{self.last_name}')"
 
 def create_author(last_name, first_name=None, middle_name=None):
     #This assumes that last_name is always bound
-    if last_name is None: print("Error New person must have a last name") # This should exit the function
+    if last_name is None:
+        print("Error New person must have a last name") # This should exit the function
+        return None
     author_iri = make_instance(str(uuid.uuid4()), person_class)
     put_value(author_iri, last_name_prop, last_name)
     if middle_name is not None: put_value(author_iri, middle_name_prop, middle_name)
     if first_name is not None:
         put_value(author_iri, first_name_prop, first_name)
+        put_value(author_iri, last_name_prop, last_name)
         put_value(author_iri, rdfs_label_property,  first_name + " " + last_name)
     else:
+        put_value(author_iri, last_name_prop, last_name)
         put_value(author_iri, rdfs_label_property, last_name)
     return author_iri
 
@@ -51,16 +62,29 @@ def find_author_string(document):
         return False
     else: return document_author_string
 
+#Loops through all the instances of Document
 def make_author_objects():
     documents = find_instances_of_class(document_class)
     for document in documents:
         document_authors = find_author_string(document)
         print(document_authors)
-        if document_authors: parse_authors(convert_to_string(document_authors))
+        if document_authors:
+            author_list = parse_authors(convert_to_string(document_authors))
+            for author_pobject in author_list:
+                find_or_create_author(author_pobject, document)
+
+def find_or_create_author(author_pobject, document):
+    author_object = find_object_from_label("Stub Value")
+    if author_object:
+        put_value(document, author_property, author_object)
+    else:
+        author_object = create_author(author_pobject.last_name, author_pobject.first_name, author_pobject.middle_name)
+        put_value(document, author_property, author_object)
+
 
 
 #make_author_objects()
-parse_authors("Justin Farrell")
+#parse_authors("Justin Farrell")
 
 
 
