5249c0c6c6ba15df0a77c2f2b6c87398af3f9c33
Michael DeBellis <mdebellissf@gmail.com>
2025-05-15 06:53:05 -0700

    New UI file

--- src/ui_md.py
+++ src/ui_md.py
@@ -28,9 +28,11 @@ def build_query(user_question):
         query_string1 = "PREFIX franzOption_openaiApiKey: " + oaik
         query_string1 = query_string1 + "PREFIX : <https://www.michaeldebellis.com/climate_obstruction/> "
         query_string1 = query_string1 + "PREFIX llm: <http://franz.com/ns/allegrograph/8.0.0/llm/> "
+        query_string1 = query_string1 + "PREFIX vdbp: <http://franz.com/vdb/prop/> "
         query_string1 = query_string1 + "SELECT  * WHERE {bind(\"" + user_question
         query_string2 = "\" as ?query) "
-        query_string2 = query_string2 + "(?response ?score ?citation ?content) llm:askMyDocuments (?query \"climate_obstruction\" 4 0.7).} "
+        query_string2 = query_string2 + "(?response ?score ?vec ?content) llm:askMyDocuments (?query \"climate_obstruction\" 4 0.7). "
+        query_string2 = query_string2 + " ?doc ?prop ?content. FILTER(isIRI(?doc)) OPTIONAL { ?doc :has_Topic ?topic}}"
         query_string = query_string1 + query_string2
         return query_string
 
@@ -59,8 +61,7 @@ st.page_link("http://localhost:10035", label="View answer graph in Gruff", icon=
 
 #st.write("This is what the first text box entered " + str(dentistInput))
 #st.write("This is what the second box wrote " + str(sparqlQuery))
-#print(do_query("Does AB restorative in Class II cavities without adhesive system, result in an acceptable failure frequency after a one-year period?"))
 
 # streamlit run C:\Users\mdebe\Documents\GitHub\Climate_Obstruction\src\ui_md.py
 
-print(build_query("What is the airspeed of an African swallow?"))
+print(build_query("Who are major supporters of climate obstruction"))
